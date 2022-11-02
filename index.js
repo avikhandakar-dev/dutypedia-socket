53660586338ea369fb83d5ac8216b336a2335f4b
avikhandakar-dev <avikhandakar@gmail.com>
2022-11-02 17:09:56 +0600

    bug fix

--- index.js
+++ index.js
@@ -7,22 +7,44 @@ const io = require("socket.io")(8000, {
 //Socket.io
 let users = [];
 
-const addUser = (userId, socketId) => {
-  !users.some((user) => user.userId === userId) &&
-    users.push({ userId, socketId });
-};
-
 const removeUser = (socketId) => {
-  users = users.filter((user) => user.socketId !== socketId);
+  users.forEach((element, index) => {
+    if (element.socketId?.includes(socketId)) {
+      if (users[index].socketId?.length > 1) {
+        const tmpSocketIds = users[index].socketId?.filter(
+          (s) => s !== socketId
+        );
+        return (users[index].socketId = tmpSocketIds);
+      } else {
+        return (users = users.filter(
+          (user) => !user.socketId?.includes(socketId)
+        ));
+      }
+    }
+  });
 };
 
 const getUser = (userId) => {
   return users.find((user) => user.userId === userId);
 };
 
+const addUser = (userId, socketId) => {
+  if (users.some((user) => user.socketId?.includes(socketId))) {
+    return;
+  }
+  if (users.some((user) => user.userId === userId)) {
+    users.forEach((element, index) => {
+      if (element.userId === userId) {
+        users[index].socketId.push(socketId);
+      }
+    });
+  } else {
+    users.push({ userId, socketId: [socketId] });
+  }
+};
+
 io.on("connection", (socket) => {
   console.log("user connected", socket.id);
-  io.emit("connected", socket.id);
 
   socket.on("join", (userId) => {
     addUser(userId, socket.id);
@@ -31,25 +53,33 @@ io.on("connection", (socket) => {
   });
   socket.on("notificationSend", (receiverId) => {
     const user = getUser(receiverId);
-    io.to(user?.socketId).emit("notificationReceived");
+    user?.socketId.forEach((id) => {
+      io.to(id).emit("notificationReceived");
+    });
   });
   socket.on("sendMessage", ({ senderId, receiverId, message }) => {
     const user = getUser(receiverId);
-    io.to(user?.socketId).emit("getMessage", {
-      senderId,
-      message,
+    user?.socketId.forEach((id) => {
+      io.to(id).emit("getMessage", {
+        senderId,
+        message,
+      });
     });
   });
   socket.on("newOrder", ({ receiverId, order }) => {
     const user = getUser(receiverId);
-    io.to(user?.socketId).emit("getOrder", {
-      order,
+    user?.socketId.forEach((id) => {
+      io.to(id).emit("getOrder", {
+        order,
+      });
     });
   });
   socket.on("updateOrder", ({ receiverId, order }) => {
     const user = getUser(receiverId);
-    io.to(user?.socketId).emit("updateOrder", {
-      order,
+    user?.socketId.forEach((id) => {
+      io.to(id).emit("updateOrder", {
+        order,
+      });
     });
   });
   socket.on("disconnect", () => {
